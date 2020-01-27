e4cda3bba1753cd6ab32c8b507e5606921887270
Ross Mawdsley <rossmawd@gmail.com>
2020-01-27 19:44:12 +0000

    adding styles,

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,11 +1,19 @@
 import React from 'react'
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Button, Text, TextInput } from 'react-native';
 
 
 const StartGameScreen = props => {
   return (
-    <View>
-      <Text>This is the Game Screen</Text>
+    <View style={styles.screen}>
+      <Text style={styles.title}>Start a new game!</Text>
+      <View style={styles.inputContainer}>
+        <Text>Select a Number</Text>
+        <TextInput />>
+       <View style={styles.buttonContainer}>
+          <Button title='reset' onPress={() => { }} />
+          <Button title='confirm' onPress={() => { }} />
+        </View>
+      </View>
     </View>
   )
 }
@@ -14,16 +22,30 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 10,
-    alignItems: 'centre',
+    alignItems: 'center'
+  },
+  title: {
+
+  },
+  inputContainer: {
+
+  },
+  buttonContainer: {
+
   }
 })
 
 export default StartGameScreen
 
 
-// puts a screen value into the styles objet
-  // flex should be 1
-  // padding 10
-  // alignItems centre
-  // justify content is default (flex start)
-  //impot this into app and put into App.js
+ //add all styles to styles
+ // for title:
+  // fontSize is 20
+  //marginVertical 10
+
+  //buttonContainer  should have:
+    // flexDirection: row
+    //width 100%
+    //justifyContent space-betweeen
+    //paddingHorizontal 15
+
